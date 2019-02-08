d1757e3bc5f43627c4fe8b10d0d50485f4d87a38
Prasanth Abraham <santh96@yahoo.com>
2019-02-07 18:17:14 -0800

    populates UI (needs polish)

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +59,16 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich swich) {
+        TextView akaTextView = (TextView) findViewById(R.id.also_known_tv);
+        TextView ingredientsTextView = (TextView) findViewById(R.id.ingredients_tv);
+        TextView descriptionTextView = (TextView) findViewById(R.id.description_tv);
+        TextView originTextView = (TextView) findViewById(R.id.origin_tv);
+
+        akaTextView.setText(swich.getAlsoKnownAs().toString());
+        ingredientsTextView.setText(swich.getIngredients().toString());
+        descriptionTextView.setText(swich.getDescription().toString());
+        originTextView.setText(swich.getPlaceOfOrigin().toString());
 
     }
 }
